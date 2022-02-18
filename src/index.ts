be226b878a462b1baaaf634608076ae588c41d75
Maciej Opaliński <poprostumieciek@gmail.com>
2022-02-18 12:21:11 +0100

    feat: Add request parsing middleware

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ const { PORT } = env;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello, world!'
