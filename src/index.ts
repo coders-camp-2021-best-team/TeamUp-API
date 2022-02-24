dc76aaa8470f5d14dbd18aae752f0c200af80ff2
Maciej Opaliński <poprostumieciek@gmail.com>
2022-02-24 19:39:35 +0100

    feat: Update `index.ts`

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,33 @@ import express from 'express';
 import { getConnectionOptions, createConnection } from 'typeorm';
 import { WinstonAdaptor } from 'typeorm-logger-adaptor/logger/winston';
 
+import { funnyHeaderMiddleware } from './common/middlewares';
+import { HelloWorldRouter } from './hello-world';
+
 import logger from './logger';
 import env from './config';
 const { PORT } = env;
 
-const app = express();
+const server = () => {
+    const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello, world!'
+    app.use(funnyHeaderMiddleware);
+
+    app.use('/hello-world', HelloWorldRouter);
+
+    app.listen(PORT, () => {
+        logger.info(`Server listening on port ${PORT}`);
     });
-});
+};
 
-getConnectionOptions().then((connectionOptions) => {
-    return createConnection({
-        ...connectionOptions,
-        logger: new WinstonAdaptor(logger, 'all')
-    });
-});
-
-app.listen(PORT, () => {
-    logger.info(`Server listening on port ${PORT}`);
-});
+getConnectionOptions()
+    .then((connectionOptions) => {
+        return createConnection({
+            ...connectionOptions,
+            logger: new WinstonAdaptor(logger, 'all')
+        });
+    })
+    .then(server);
