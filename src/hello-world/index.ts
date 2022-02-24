9cd677996bf8ee22072f09dee3a6704240334b77
Maciej Opaliński <poprostumieciek@gmail.com>
2022-02-24 19:39:23 +0100

    feat: Add common exports for `HelloWorld` module

---
+++ src/hello-world/index.ts
@@ -0,0 +1,5 @@
+export * from './greeting.dto';
+export * from './greeting.entity';
+export * from './hello-world.controller';
+export * from './hello-world.router';
+export * from './hello-world.service';
