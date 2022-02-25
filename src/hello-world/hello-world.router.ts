c5b7d527857f2bb32e4b5552ed29acab68b9ba8c
Maciej Opaliński <poprostumieciek@gmail.com>
2022-02-25 22:46:19 +0100

    fix: Remove `HelloWorldRouter`

--- src/hello-world/hello-world.router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { HelloWorldController } from '.';
-
-export const HelloWorldRouter = Router();
-
-HelloWorldRouter.get('/greet', HelloWorldController.greet);
-HelloWorldRouter.get('/greetings', HelloWorldController.getAllGreetings);
-HelloWorldRouter.post('/greetings', HelloWorldController.createGreeting);
--- src/hello-world/index.ts
+++ src/hello-world/index.ts
@@ -1,5 +1,4 @@
 export * from './greeting.dto';
 export * from './greeting.entity';
 export * from './hello-world.controller';
-export * from './hello-world.router';
 export * from './hello-world.service';
