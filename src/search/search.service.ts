8c6a9f8547a2776724a003e69f2cb1a086785050
Maciej Opaliński <poprostumieciek@gmail.com>
2022-03-22 22:10:59 +0100

    fix: Fix search module

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -1,20 +1,23 @@
-import { Like } from 'typeorm';
-import { UserSkill } from '../user';
+import { ILike } from 'typeorm';
+import { User } from '../user';
 export const SearchService = new (class {
     async getResults(search: string, take = 20, skip = 0) {
-        const results = await UserSkill.find({
-            where: {
-                game: {
-                    name: Like(`%${search}%`)
+        const results = await User.find({
+            where: [
+                {
+                    first_name: ILike(`%${search}%`)
+                },
+                {
+                    last_name: ILike(`%${search}%`)
+                },
+                {
+                    username: ILike(`%${search}%`)
                 }
-            },
-            take: take,
-            skip: skip
+            ],
+            take,
+            skip
         });
 
-        if (!results.length) {
-            return null;
-        }
         return results;
     }
 })();
