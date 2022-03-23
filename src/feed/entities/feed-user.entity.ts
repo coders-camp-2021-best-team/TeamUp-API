32f5107efdcb481a3fd52333e2510ac90a9a1a4f
Maciej Opaliński <poprostumieciek@gmail.com>
2022-03-23 15:59:52 +0100

    feat: Add `FeedUser` entity

---
+++ src/feed/entities/feed-user.entity.ts
@@ -0,0 +1,29 @@
+import {
+    BaseEntity,
+    Column,
+    Entity,
+    ManyToOne,
+    PrimaryGeneratedColumn
+} from 'typeorm';
+import { User } from '../../user';
+import { Feed } from './feed.entity';
+
+@Entity('feed_users')
+export class FeedUser extends BaseEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @ManyToOne(() => Feed, (f) => f.recommendedUsers, {
+        onDelete: 'CASCADE'
+    })
+    feed: Feed;
+
+    @ManyToOne(() => User, { eager: true, onDelete: 'CASCADE' })
+    user: User;
+
+    @Column('int')
+    common_skills: number;
+
+    @Column('bool', { default: false })
+    swiped: boolean;
+}
