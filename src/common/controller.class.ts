dcf59533cc3a020beeec63c56fbe7a6cae27da4a
Maciej Opaliński <poprostumieciek@gmail.com>
2022-03-23 21:28:42 +0100

    feat: Add children controllers to base Controller class

--- src/common/controller.class.ts
+++ src/common/controller.class.ts
@@ -1,9 +1,18 @@
 import { Router } from 'express';
+import logger from '../logger';
 
 export abstract class Controller {
     private router = Router();
 
-    constructor(private base_path: string) {}
+    constructor(private base_path: string, private controllers?: Controller[]) {
+        this.controllers?.forEach((controller) => {
+            this.router.use(controller.getBasePath(), controller.getRouter());
+
+            logger.info(
+                `controller: ${controller.constructor.name} initialized`
+            );
+        });
+    }
 
     public getBasePath() {
         return this.base_path;
