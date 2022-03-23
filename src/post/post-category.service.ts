0a516843fc3f6736c650b3bc213c8170132384da
Maciej Opaliński <poprostumieciek@gmail.com>
2022-03-23 21:29:50 +0100

    feat: Add `PostCategoryService`

---
+++ src/post/post-category.service.ts
@@ -0,0 +1,47 @@
+import { FindConditions, ILike } from 'typeorm';
+import {
+    QueryPostDto,
+    PostCategory,
+    CreateCategoryDto,
+    UpdateCategoryDto
+} from '.';
+
+export const PostCategoryService = new (class {
+    async getCategories({ q, skip, take, sort }: QueryPostDto) {
+        const cond = ILike(`%${q}%`);
+        const where: FindConditions<PostCategory> | undefined = q
+            ? { name: cond }
+            : undefined;
+
+        return PostCategory.find({
+            where,
+            skip,
+            take,
+            order: { name: sort }
+        });
+    }
+
+    async createCategory(data: CreateCategoryDto) {
+        const cat = new PostCategory();
+        cat.name = data.name;
+        return cat.save();
+    }
+
+    async updateCategory(catID: string, body: UpdateCategoryDto) {
+        const cat = await PostCategory.findOne(catID);
+
+        if (!cat) return null;
+
+        cat.name = body.name || cat.name;
+
+        return cat.save();
+    }
+
+    async removeCategory(catID: string) {
+        const post = await PostCategory.findOne(catID);
+
+        if (!post) return null;
+
+        return post.remove();
+    }
+})();
