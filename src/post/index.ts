168cf632b273bf0479cff0aae54bb85359df5874
Maciej Opaliński <poprostumieciek@gmail.com>
2022-03-23 21:30:19 +0100

    feat: Add exports for `PostModule`

---
+++ src/post/index.ts
@@ -0,0 +1,7 @@
+export * from './dto/';
+export * from './entities/';
+
+export * from './post.controller';
+export * from './post-category.controller';
+export * from './post.service';
+export * from './post-category.service';
