dce2084f914e76cbab0408d7e1ab929b57c67847
Maciej Opaliński <poprostumieciek@gmail.com>
2022-04-07 20:36:31 +0200

    fix: Add `localhost:3000` origin to CORS config

--- src/common/api.class.ts
+++ src/common/api.class.ts
@@ -60,7 +60,11 @@ export class API {
     private initCORS() {
         this.app.use(
             cors({
-                origin: [CLIENT_URL, 'http://localhost']
+                origin: [
+                    CLIENT_URL,
+                    'http://localhost',
+                    'http://localhost:3000'
+                ]
             })
         );
     }
