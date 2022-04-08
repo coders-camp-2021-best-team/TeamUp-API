c7ebf695e6143eeace0acfda5ccc36072572a8eb
Maciej Opaliński <poprostumieciek@gmail.com>
2022-04-08 18:21:25 +0200

    fix: Fix circular dependency error

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,6 +1,6 @@
-// eslint-disable-next-line prettier/prettier
-export * from './api.class';
+// eslint-disable-next-line simple-import-sort/exports
 export * from './controller.class';
+export * from './api.class';
 export * from './exceptions/';
 export * from './interfaces/';
 export * from './middlewares/';
